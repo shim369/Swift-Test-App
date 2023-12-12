43e70dfcbe1c44698ea4deabf23e6a04bb85a9a5
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-12 16:55:33 +0900

    Initial Commit

---
+++ uranai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uranai
+//
+//  Created by ohtani shimpei on 2023/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
