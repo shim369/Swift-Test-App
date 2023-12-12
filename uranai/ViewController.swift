ac75a13373ec09987d743cece845731a678ebdf6
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-12 18:39:57 +0900

    update function

--- uranai/ViewController.swift
+++ uranai/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func getOmikuji(_ sender: Any) {
+        let results = ["大吉","中吉","吉","大凶"]
+        let random = arc4random_uniform(UInt32(results.count))
+        
+        self.myLabel.text = results[Int(random)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.layer.borderColor = UIColor.orange.cgColor
+        myLabel.layer.borderWidth = 5
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = myLabel.bounds.width / 2
     }
 
 
